cc632ec97f6e08a67e34a5c3ec33c144319f90a7
Matthias-Kelvin Daous <mkdaous@gmail.com>
2015-07-11 16:29:07 +0200

    update use maven add commons-codec & start log user

--- src/Login.java
+++ src/Login.java
@@ -1,12 +1,22 @@
 
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
 
 /**
  * Servlet implementation class Login
@@ -39,7 +49,61 @@ public class Login extends HttpServlet {
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		
-		HttpSession session = request.getSession(true);    
+		HttpSession session = request.getSession(true);  
+
+		try {
+
+		    Class.forName( "com.mysql.jdbc.Driver" );
+		   
+		    /* Connexion à la base de données */
+		    String url = "jdbc:mysql://localhost:3306/school_doodle";
+		    String utilisateur = "root";
+		    String motDePasse = "root";
+		    java.sql.Connection connexion = null;
+		    try {
+		        connexion = DriverManager.getConnection( url, utilisateur, motDePasse );
+
+		        String sql = "select * from users where username=? and password=?;" ;
+		        		
+		        PreparedStatement preparedStatement =
+		                (PreparedStatement) connexion.prepareStatement(sql);
+
+		        preparedStatement.setString(1, username);
+		        System.out.println(DigestUtils.sha1Hex("soleil"));
+		        preparedStatement.setString(2, DigestUtils.sha1Hex("soleil"));
+		        //preparedStatement.setLong  (3, 123);
+
+		        ResultSet users = preparedStatement.executeQuery();
+		        
+		        while (users.next()){
+		        	System.out.println(users.getString(2));
+		        	
+		        }
+		        System.out.println("It works ! ");
+
+		    } catch ( SQLException e ) {
+
+		        System.out.println("cerise !! ");
+
+		        /* Gérer les éventuelles erreurs ici */
+		    } finally {
+		        if ( connexion != null )
+		            try {
+		                connexion.close();
+		            } catch ( SQLException ignore ) {
+
+				        System.out.println("poire ! ");
+
+		            }
+		    }
+
+		} catch ( ClassNotFoundException e ) {
+
+	        System.out.println("pomme! ");
+
+
+		}
+		
 		
 		if (username.compareTo("username") == 0 && password.compareTo("p") == 0) {
 			session.setAttribute("username", username);
