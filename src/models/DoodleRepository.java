7b0e4180b64a834a620c13b2503d20d70563e6fb
Matthias-Kelvin Daous <mkdaous@gmail.com>
2015-07-19 10:37:36 +0200

    create findById method

--- src/models/DoodleRepository.java
+++ src/models/DoodleRepository.java
@@ -1,6 +1,8 @@
 package models;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -8,6 +10,8 @@ import java.util.Date;
 import org.apache.commons.codec.digest.DigestUtils;
 
 import com.mysql.jdbc.PreparedStatement;
+
+import entities.Doodle;
 
 
 
@@ -17,12 +21,12 @@ public class DoodleRepository extends AbstractRepository{
 		super("doodles");
 	}
 
-	public boolean save(int uid, int caid, int status, String question) {
+	public int save(int uid, int caid, int status, String question) {
 		try {
 			String sql = "INSERT INTO `doodles`(`uid`, `caid`, `status`, `question`, `token`) VALUES (?, ?, ?, ?, ?)" ;
     		
 			PreparedStatement preparedStatement =
-					(PreparedStatement) this.connexion.prepareStatement(sql);
+					(PreparedStatement) this.connexion.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
 			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 			Date now = new Date();
@@ -34,16 +38,53 @@ public class DoodleRepository extends AbstractRepository{
 			preparedStatement.setString(4, question);
 			preparedStatement.setString(5, dateStr);
 			
-			int rowsAffected = preparedStatement.executeUpdate();
-		        			
-			if (rowsAffected > 0){
-				return true;
-			}
+			int rowAffected = preparedStatement.executeUpdate();
+			
+					
+		     if (rowAffected == 0) {
+		    	 throw new SQLException("Creating user failed, no rows affected.");
+		     }
+			
+			  try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+		            if (generatedKeys.next()) {
+		                return (int) generatedKeys.getLong(1);
+		            }
+		            else {
+		                throw new SQLException("Creating user failed, no ID obtained.");
+		            }
+		        }
 			
 		} catch (Exception e) {
 			System.err.println(e.toString());
 		}
 		
-		return false;	
+		return 0;	
+	}
+	
+	public Doodle findById(int did) {
+		try {
+			String sql = "SELECT * FROM doodles WHERE did=?;" ;
+    		
+			PreparedStatement preparedStatement =
+					(PreparedStatement) this.connexion.prepareStatement(sql);
+						
+			preparedStatement.setInt(1, did);
+			
+			ResultSet item = preparedStatement.executeQuery();
+			
+			if (item.next()) {
+				return new Doodle(
+							Integer.parseInt(item.getString("did")), 
+							Integer.parseInt(item.getString("uid")),
+							Integer.parseInt(item.getString("caid")), 
+							Integer.parseInt(item.getString("status")), 
+							item.getString("question"), 
+							item.getString("token"));
+			}
+		} catch (Exception e) {
+			System.err.println(e.toString());
+		}
+	
+		return null;
 	}
 }
