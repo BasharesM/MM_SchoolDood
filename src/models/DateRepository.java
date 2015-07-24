8bdab12fd2762620ecd1b39191aa4b6307229a14
Matthias-Kelvin Daous <mkdaous@gmail.com>
2015-07-24 23:59:15 +0200

    save a collection of date

--- src/models/DateRepository.java
+++ src/models/DateRepository.java
@@ -1,6 +1,9 @@
 package models;
 
 import java.sql.ResultSet;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 import com.mysql.jdbc.PreparedStatement;
 
@@ -41,5 +44,33 @@ public class DateRepository extends AbstractRepository{
 		}
 	
 		return null;
+	}
+
+	public boolean saveAll(Dates dates) {
+		boolean status = true;
+		
+		try {
+			for (int i = 0 ; i < dates.size() ; i++) {
+				Date date = dates.get(i);
+				
+				String sql = "INSERT INTO dates (did, heure, date) VALUES (?, ?, ?);";
+				
+				PreparedStatement preparedStatement = (PreparedStatement) this.connexion.prepareStatement(sql);
+					
+				preparedStatement.setInt(1, date.getDid());
+				preparedStatement.setString(2, date.getHeure());
+				preparedStatement.setString(3, date.getDate());
+				
+				int rowsAffected = preparedStatement.executeUpdate();
+				
+				if (rowsAffected != 1) {
+					status = false;
+				}
+			}
+		} catch (Exception e) {
+			System.err.println(e.toString());
+		}
+		
+		return status;
 	}
 }
